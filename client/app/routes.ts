89e57c4f7906c846be6d71574bc68a7effee1cb1
Steven Hagene <steven@semperdigitalsolutions.com>
2025-06-15 09:28:26 -0400

    updated fe layout

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -1,7 +1,11 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
+import { layout } from "@react-router/dev/routes";
+
 export default [
-  index("routes/home.tsx"),
-  route("/2ndPage", "routes/2ndPage.tsx")
+  layout("layouts/AppLayout.tsx", [
+    index("routes/home.tsx"),
+    route("/2ndPage", "routes/2ndPage.tsx"),
+  ]),
 ] satisfies RouteConfig;
 
